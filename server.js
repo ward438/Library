432f2c187f1eed584a11fa460a23de7a735d0bfc
ward438 <75048548+ward438@users.noreply.github.com>
2021-06-24 11:42:21 -0500

    building server, next step mongoDB

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
-// Define middleware here
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+// Serve up heroku static assets 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -14,10 +16,12 @@ if (process.env.NODE_ENV === "production") {
 // Define API routes here
 
 // Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// api route
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
