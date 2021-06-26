8a876d9b4d1bb32173862498aac860a494f9772a
ward438 <75048548+ward438@users.noreply.github.com>
2021-06-26 17:08:43 -0500

    deploying

--- client/src/utils/routing.js
+++ client/src/utils/routing.js
@@ -1,13 +1,11 @@
 import { Switch, Route } from "react-router-dom";
 // import SearchResults from '../pages/searchResults'
 import Home from '../pages/home';
-import GetBooks from '../utils/apiCall'
 
 export function Routing() {
     return (
         <Switch>
-            <Route path="/" component={Home} />
-            {/* <Route path='/api/Books' component={Home} /> */}
+            <Route path="/" component={Home} />          
             <Route render={() => <h1>Page not found</h1>} />
         </Switch>
     );
